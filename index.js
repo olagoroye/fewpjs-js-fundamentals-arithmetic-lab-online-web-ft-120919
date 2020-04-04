4163b95f66ef9d357b41fee96ae0bb416592eb6d
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-04-04 10:35:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+var multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+// let random =5;
+// integer = 0;
+
+// random  > integer;
+const random = Math.floor(Math.random() *10) + 1;
+const mod = 18 % 7;
+
+const max = Math.max(1, 2, 3, 4, 20);
+
+
+
+
+
+ 
+  
+
